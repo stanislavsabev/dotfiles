d58b7fe7ba4c70205eb8e369490953c870586c72
stanislavsabev <bezraboten.34@gmail.com>
2023-03-25 00:00:04 +0100

    Update fish wtadd on linux

--- linux/fish/functions/wtadd.fish
+++ linux/fish/functions/wtadd.fish
@@ -1,13 +1,20 @@
 
 function wtadd
-    set -l _usage "usage: wt-add [NEW_BRANCH] [PATH] COMMIT-ISH"
-    argparse 'h/help' -- $argv
-    or return
+    set -f _name wt-add
+    set -l _usage "usage: $_name [-x | -d] [NEW_WORKTREE] [PATH] COMMIT-ISH
+    Add worktree-branch to current project.
+    
+    -x --ex         Extend. Copies helper directories (like .vscode) from parent dir.
+                    Cannot be used with `--dry-run`
+    -d --dry-run    Print the command that would run.
+                    Cannot be used with `-x`
+    "
+    argparse -n -x x,d $_name 'h/help' 'x/ex' 'd/dry-run' -- $argv
     set -l last_status $status
 
     if set -ql _flag_help
         or test $last_status -ne 0
-        echo "usage: wt-add"
+        echo $_usage
         return $last_status
     end
 
@@ -21,9 +28,20 @@ function wtadd
         case 3
             set argstr -b $argv[1] $argv[2] $argv[3]
         case '*'
-            echo "wt-add: invalid arguments"
+            echo "$_name: invalid arguments"
             echo $_usage
             return $invalid_arguments
-        end
-    command git worktree add $argstr
+    end
+
+    set -l cmd git
+    if set -ql _flag_dry_run
+        set -p cmd echo
+    end
+
+    command $_cmd worktree add $argstr
+
+    if set -ql _flag_ex
+        vscode_add_sett
+    end
+
 end
