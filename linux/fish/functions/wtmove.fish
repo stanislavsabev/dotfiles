2c8fd860ab1608de3ba2e8c58f15c5664877afca
stanislavsabev <bezraboten.34@gmail.com>
2023-03-25 00:04:04 +0100

    Update fish wtmove on linux after the macos version

--- linux/fish/functions/wtmove.fish
+++ linux/fish/functions/wtmove.fish
@@ -1,11 +1,26 @@
-function wtmove
-    if test (count $argv) -ne 2
-        echo "usage: wt-mv BRANCH NEW_BRANCH"
+function wtmove --description "Rename worktree"
+    set -l options (fish_opt -s h -l help)
+    set options $options (fish_opt -s d -l dry-run --long-only)
+    argparse $options -- $argv
+
+    if set -ql _flag_help
+        or test (count $argv) -ne 2
+        echo "usage: wt-mv [--dry-run] WORKTREE NEW_WORKTREE
+        
+    --dry-run   print commands that would be executed
+    "
         return $invalid_arguments
     end
-
-    git worktree move $argv[1] $argv[2]
-    cd $argv[2]
-    git branch -M $argv[2]
-    cd -
+    
+    set -l git_cmd git
+    set -l cd_cmd cd
+    if set -ql _flag_dry_run
+        set git_cmd -p echo "dry run:"
+        set cd_cmd -p echo "dry run:"
+    end
+    
+    command $git_cmd worktree move $argv[1] $argv[2]
+    $cd_cmd $argv[2]
+    command $git_cmd branch -M $argv[2]
+    $cd_cmd -
 end
