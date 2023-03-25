d875fc10dff89dcbb4a4c9f67ae25091a50c5cdb
stanislavsabev <bezraboten.34@gmail.com>
2023-03-25 01:59:58 +0100

    Update sync_config.fish on linux

--- linux/fish/functions/sync_config.fish
+++ linux/fish/functions/sync_config.fish
@@ -23,6 +23,7 @@ function sync_config --description "Sync config files between dotfiles repo (SRC
     set -f SRC
     set -f DEST
     set -f FLAGS
+    set -f _suffix '.bak'
     set names $argv
     
     for name in $names
@@ -35,11 +36,11 @@ function sync_config --description "Sync config files between dotfiles repo (SRC
             case bashrc
                 set SRC "$HOME/.bashrc"
                 set DEST "$DOTFILES_DIR/bash/.bashrc"
-                set FLAGS -b --suffix='bak'
+                set FLAGS -b --suffix=$_suffix
             case vscode
                 set SRC "$CONFIG_DIR/Code/User"
                 set DEST "$DOTFILES_DIR/vscode"
-                set FLAGS --include="snippets/" --include "*.json" --include "*.code-snippets" --exclude "*" -b --suffix='bak'
+                set FLAGS --include="snippets/" --include "*.json" --include "*.code-snippets" --exclude "*" -b --suffix=$_suffix
             case git
                 set SRC $HOME
                 set DEST "$DOTFILES_DIR/git"
@@ -47,7 +48,7 @@ function sync_config --description "Sync config files between dotfiles repo (SRC
             case tmux
                 set SRC "$HOME/.tmux.conf"
                 set DEST "$DOTFILES_DIR/.tmux.conf"
-                set FLAGS -b --suffix='bak'
+                set FLAGS -b --suffix=$_suffix
             case '*'
                 echo "sync_config: Unkown name: $name"
                 return $invalid_arguments
