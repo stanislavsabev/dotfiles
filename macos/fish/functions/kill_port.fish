1a922f0c6a49e7f7221e8e6ca60b195eb1fe489f
stanislavsabev <bezraboten.34@gmail.com>
2023-04-07 22:52:31 +0200

    Remove macos/fish

--- macos/fish/functions/kill_port.fish
+++
@@ -1,7 +0,0 @@
-function kill_port
-
-    for port in (lsof -t -i:$argv[1])
-        kill -9 $port
-        echo "killed $port"
-    end
-end
--- macos/fish/functions/vscode_clear_all.fish
+++
@@ -1,5 +0,0 @@
-function vscode-clear-cache
-    rm -rf '~/Library/Application Support/Code/Cache/*'
-    rm -rf '~/Library/Application Support/Code/CachedData/*'
-    vscode_clear_pycache $argv
-end
