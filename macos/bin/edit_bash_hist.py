b165e0f9039264b782d07958a6a058bde13ae673
stanislavsabev <bezraboten.34@gmail.com>
2023-04-17 10:31:22 +0200

    Update edit_bash_history script in macos/bin

--- macos/bin/edit_bash_hist.py
+++ macos/bin/edit_bash_hist.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python3
+"""Edit bash history (prototype)"""
 
 
 def main():
+    """Edit bash history (prototype)"""
     seen = set()
     out = []
 
     with open("bash_history", "r", encoding='utf-8') as fd:
         lines = fd.readlines()
-    
+
     rm = False
     for line in reversed(lines):
         if line == '\n':
@@ -21,9 +24,10 @@ def main():
         rm = False
         seen.add(line)
         out.append(line)
-    
-    with open("bash_history", "w", encoding='utf-8') as fd: 
+
+    with open("bash_history", "w", encoding='utf-8') as fd:
         fd.writelines(reversed(out))
+
 
 if __name__ == '__main__':
     main()
