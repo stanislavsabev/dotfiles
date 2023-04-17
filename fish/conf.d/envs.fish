b79804082c283932e3c1736f878e6abf79e6444a
stanislavsabev <bezraboten.34@gmail.com>
2023-04-17 09:21:46 +0200

    Update fish with sql proj

--- fish/conf.d/envs.fish
+++ fish/conf.d/envs.fish
@@ -29,7 +29,8 @@ switch (uname)
         set -gx Q2C_DIR "$PROJECTS_DIR/q2c"
         set -gx FE_DIR "$PROJECTS_DIR/ggrc_fe"
         set -gx SOA_DIR "$PROJECTS_DIR/soa"
-
+        set -gx SQL_DIR "$PROJECTS_DIR/sql"
+        
         # Python
         set -gx VENV_MIG_NAME ".vmig"
         set -gx VENV_BE_NAME ".vbe"
