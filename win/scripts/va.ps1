2002fd3eebde4e8ddbb3b970f52c1d6e3268be63
stanislavsabev <bezraboten.34@gmail.com>
2023-06-04 13:15:43 +0200

    WIP | Add .ps1 scripts

---
+++ win/scripts/va.ps1
@@ -0,0 +1,54 @@
+# set VA=
+# if "%1"=="" (
+#     set VA=.\.venv
+#  ) else (
+#     set VA="%1%"
+# )
+
+# %VA%\Scripts\activate.bat
+
+param(
+     [Parameter(Position = 0, Mandatory = $false)]
+     [string]$Arg1
+
+ )
+
+$VA = ".venv"
+if ($Arg1) {
+    $VA = $Arg1
+}
+
+# & "$(Split-Path $MyInvocation.MyCommand.Path)/$VA/Scripts/activate.ps1"
+Write-Host $PSScriptRoot
+# SIG # Begin signature block
+# MIIFlAYJKoZIhvcNAQcCoIIFhTCCBYECAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUJrq49Y2Tky4kvjiF0Dq2tGxQ
+# L6mgggMiMIIDHjCCAgagAwIBAgIQN3M70tg1lY9NwPFShbP+qzANBgkqhkiG9w0B
+# AQsFADAnMSUwIwYDVQQDDBxQb3dlclNoZWxsIENvZGUgU2lnbmluZyBDZXJ0MB4X
+# DTIzMDYwMjExMTUwNVoXDTI0MDYwMjExMzUwNVowJzElMCMGA1UEAwwcUG93ZXJT
+# aGVsbCBDb2RlIFNpZ25pbmcgQ2VydDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC
+# AQoCggEBALvwXOiwp2moPGR1RIJWitZS4Zkp5DdiDfIszNUbwd9nED4TRN1MFVp0
+# TcywqsapCZQRzrcR+YqWOoSJVqO6YtpCHzbZ9uV8WPMy7wl85kIljFQqm7eZoD9x
+# PcZoiy7ZASFmRO8dSfHILA6gJsQEbqenuRXHI5R9bHEF6unh0/fQUum2iylCTFjL
+# Gv0u4HQai/WOQTO+wzTELUa/b/4n6LAapVkQJh2RidC08GRgbLw0IO+9eQ/Eo2Dw
+# 4U1WChyTmd0KhKyPTgyNjvhw+SrZj5NYz8/at3Lf5Pmij8882U1hP4sAb5kfTYsD
+# n9S5r5gCWj22zBr/vBjFgqkVfkvxF7UCAwEAAaNGMEQwDgYDVR0PAQH/BAQDAgeA
+# MBMGA1UdJQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQWBBQgxlgaSC82xgTtrxpXNBr8
+# uvtEbzANBgkqhkiG9w0BAQsFAAOCAQEASSDCsvbRdHQRFi8DYafehN5yEtvfaQa9
+# escDGqldfL0nhpLlzJcEL+R667UK43b0VkKJ0XgWMJpzHnoF08nhpNyXjUaiaLsu
+# qf5RUe4E06m9rMVHsHJUF5i2t1lE3BUwVoBFaY5+iX8gBIqi/g94wWslgvBa2nux
+# dCrXGr5byt/Nv5N7BU6FAyPL8hq6m3Pd3RpK1pqjL7BA8jg38tbAakn3OnkBE5At
+# J5iocKFg41XJPQOeNRmgEs/p0THUkyCi/bdzg7CuDzlGctHexjtwpIbo0k/8B+Us
+# Cinkn5sDKpHgqvhzWnX37n+vCfpOdaEdXKIltczO7gVTZBBMG/N4lzGCAdwwggHY
+# AgEBMDswJzElMCMGA1UEAwwcUG93ZXJTaGVsbCBDb2RlIFNpZ25pbmcgQ2VydAIQ
+# N3M70tg1lY9NwPFShbP+qzAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEKMAig
+# AoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgEL
+# MQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQUG9+XxBP16QhjEIy0+TtB
+# 0rapfn4wDQYJKoZIhvcNAQEBBQAEggEArAtUEqtZvWrTGVvk4+Icb74esErlqAOx
+# +gidX42mARrtQXTiCGfz89m7Xxg6o6H7Pt/WFSLyPX8X1QGaEgABQitVeguqnqVD
+# 2dmfV55JDlDYA9v1REdUki+FEHyzwT45nWkWgPLyf6lIyDDFksiVYGT2eCPQnXK7
+# x+bdi/XDr4JoPwRkm3jrdZO+ypLK8/FlETt6EvHIDytnVNDMnoIOsvfngry+NtGj
+# H6cA8YXojKkvtHGNHf2wEQi5+eTKh8RWhS4k+/IvzFfKys9pEiRUyNxrtUIYE5as
+# NpXW5uXD5YEmD98FwDRvg3vgol0OQBwq22OU0xOtlCle7rEvJDsz4g==
+# SIG # End signature block
