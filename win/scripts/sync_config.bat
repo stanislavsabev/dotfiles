cd5a24e2afca493327898f8cad947a70297ec09a
stanislavsabev <bezraboten.34@gmail.com>
2023-06-05 10:32:45 +0200

    Refactor sync_config.bat on win/

--- win/scripts/sync_config.bat
+++ win/scripts/sync_config.bat
@@ -1,56 +1,57 @@
 @echo off
 SETLOCAL enableDelayedExpansion
 
-set XS=%DEVSETUP_DIR%\vscode
-set XD=%AppData%\Code\User
-
-if "%1"=="/r" ( 
-    set XS=%AppData%\Code\User
-    set XD=%DEVSETUP_DIR%\vscode
+set DEST=%DOTFILES_DIR%\win\vscode
+set SRC=%AppData%\Code\User
+if "%1"=="/r" (
+    set TMP_VAR=%DEST%
+    set DEST=%SRC%
+    set SRC=!TMP_VAR!
     shift
  )
 
-set flag=%1
-set SRC=!XS!
-set DEST=!XD!
+set COMND=xcopy
+if "%1"=="/D" (
+    set COMND=echo dry-run: !COMND!
+    shift
+ )
 
-if "%flag%"=="/?" ( GOTO:Syntax )
-if "%flag%"=="sett" ( GOTO:proc_sett )
-if "%flag%"=="kb" ( GOTO:proc_kb )
-if "%flag%"=="ext" ( GOTO:proc_ext )
-if "%flag%"=="snip" ( GOTO:proc_snip )
+set option=%1
 
-:Syntax
+if "%option%"=="/?" ( GOTO:Usage )
+if "%option%"=="sett" ( GOTO:proc_sett )
+if "%option%"=="kb" ( GOTO:proc_kb )
+@REM if "%option%"=="ext" ( GOTO:proc_ext )
+if "%option%"=="snip" ( GOTO:proc_snip )
+
+:Usage
     echo Usage:
-    echo    %~n0 [/r] ^<option^> [^<sub_option^>] [^<xcopy_flags^>]
+    echo    %~n0 [/r] [/D] ^<option^> [^<sub_option^>] [^<xcopy-flags^>] 
     echo.
     echo    Copies files from this repo to VSCode install dir, based on
     echo    the option value
     echo.
-    echo    FLAGS:
-    echo.
     echo      /r    Reverse copy. From VSCode install dir to this repo. 
+    echo      /D    Dry-run. Print the command, that would be executed.
     echo.
-    echo    OPTIONS:
-    echo.
-    echo      What to copy
+    echo    option What to copy
     echo.
     echo      sett  Copy 'settings.json'
     echo      kb    Copy 'keybindings.json'
     echo      ext   Copy extentions from 'G:' to %%AppData%% (slow^)
     echo      snip [sub_option ^<py/vba/my^>] ...
     echo            Copy snippets. ^If no sub option is provided, all snippets are copied
-    echo    xcopy_flags ...
-    echo            Additional flags to pass to 'xcopy'. See xcopy /?
+    echo.
+    echo    xcopy-flags Additional flags to pass to 'xcopy'. See xcopy /?
     
 GOTO:EOF
 
 :proc_sett
-    xcopy %2/y %SRC%\settings.json %DEST%\settings.json
+    echo F|%COMND% %2/y %SRC%\settings.json %DEST%\settings.json
     GOTO:EOF
 
 :proc_kb
-    xcopy %2/y %SRC%\keybindings.json %DEST%\keybindings.json
+    echo F|%COMND% %SRC%\keybindings.json %DEST%\keybindings.json %2/y
     GOTO:EOF
 
 :proc_ext
@@ -60,7 +61,7 @@ GOTO:EOF
         echo Operation cancelled...
     ) else (
         echo Copying...
-        xcopy %2/s/y/j %SRC%\extensions %USERPROFILE%\.vscode\extensions
+        %COMND% %SRC%\extensions %USERPROFILE%\.vscode\extensions %2/s/y/j
     )
     GOTO:EOF
 
@@ -74,7 +75,7 @@ GOTO:EOF
         )
     )
     :match
-    xcopy "%SRC%\snippets\%op%*" "%DEST%\snippets\%op%*" %2/s/y
+    %COMND% "%SRC%\snippets\%op%*" "%DEST%\snippets\%op%*" %2/s/y
     GOTO:EOF
 
 :End
