280e0404e70947a37c999cd27431d50dc0fd8b51
stanislavsabev <bezraboten.34@gmail.com>
2023-11-12 22:45:07 +0100

    fish: Update make_venv

--- fish/functions/make_venv.fish
+++ fish/functions/make_venv.fish
@@ -1,31 +1,30 @@
 
-function make_venv -a PATH --description "make_venv [PATH]"
-    set -l _usage "usage: make_venv [PATH]
+function make_venv -a VENV_PATH --description "make_venv [VENV_PATH]"
+    set -l _usage "usage: make_venv [VENV_PATH]
 
-    PATH  Virtual environment path. If no path is provided,
+    VENV_PATH  Virtual environment path. If no path is provided,
           the script will check .python-cfg file in current directory
           and take the path from there, finaly will use
           the default path '$VENV_NAME'
     "
     argparse h/help -- $argv
     set -l last_status $status
-    set -l argc (count $argv)
-
     if set -ql _flag_help
         or test $last_status -ne 0
         echo $_usage
         return
     end
 
-    set -l _venv
+    set -l argc (count $argv)
+    set -l venv_name
     if test $argc -eq 1
-        set _venv "$argv[1]"
+        set venv_name "$argv[1]"
     else if test -e .python-cfg
-        set _venv (head -1 .python-cfg)
+        set venv_name (head -1 .python-cfg)
     else
-        set _venv $VENV_NAME
+        set venv_name $VENV_NAME
     end
 
-    python -m venv "$_venv"
-    source "$vaname/bin/activate.fish"
+    python -m venv $venv_name
+    source "$venv_name/bin/activate.fish"
 end
