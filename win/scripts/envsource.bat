f72ee443799e1a75f2a04ae99ea2b719bb9d78f2
stanislavsabev <bezraboten.34@gmail.com>
2024-02-29 00:53:37 +0100

    win: Add envsource to scripts/

---
+++ win/scripts/envsource.bat
@@ -0,0 +1,26 @@
+@echo off
+set SELF=%~n0
+
+if [%1] == [-h] goto :usage
+if [%1] == [--help] goto :usage
+if [%1] == [] goto :invalid_args
+
+SET _FNAME=%1
+
+FOR /f "tokens=1,2 delims==" %%a in (%_FNAME%) DO (
+    echo %%a %%b
+    set %%a=%%b
+)
+
+goto:EOF
+
+:invalid_args
+    echo %SELF%: Missing file name argument, see -h for usage
+    exit /b 1
+
+:usage
+    echo usage: %SELF% [-h] ENV_FILE
+    echo.  Set env variables from file
+    echo.
+    echo    -h --help       Print this message
+    echo    ENV_FILE        Env file name
