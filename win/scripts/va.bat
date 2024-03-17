4ebe8b5914d47f1d36a98ec9a2ea3e17eacd396f
stanislavsabev <bezraboten.34@gmail.com>
2024-03-17 01:45:24 +0100

    win: Update va.bat script

--- win/scripts/va.bat
+++ win/scripts/va.bat
@@ -6,9 +6,12 @@ if [%1] == [--help] goto :usage
 
 set VA=
 SET PYTHON_CFG=.python-cfg
+SET PROJ_CFG=.proj-cfg
 
 if not [%1] == [] (
     set VA="%1"
+) else if exist %PROJ_CFG% (
+    call :read_proj_cfg
 ) else if exist %PYTHON_CFG% (
     call :read_python_cfg
 )
@@ -25,6 +28,15 @@ goto :EOF
     for /F "tokens=* USEBACKQ" %%F IN ( `head 1 %PYTHON_CFG%` ) do (
         SET VA=%%F
         exit /b 0
+    )
+    exit /b 1
+
+:read_proj_cfg
+    for /F "tokens=1,2 delims==" %%a IN ( %PROJ_CFG% ) do (
+        if ["%%a"] == ["VENV_PATH"] (
+            SET VA=%%b
+            exit /b 0
+        )
     )
     exit /b 1
 
